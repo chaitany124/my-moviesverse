f163668e816e64a7f5f07e17b2a2fdad2f600ce8
chaitany124 <c123ha12@gmail.com>
2025-05-25 15:53:37 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,26 @@
+const movies = [
+  {
+    id: '1',
+    title: 'Wonder Woman 1984',
+    year: '2020',
+    poster: 'https://image.tmdb.org/t/p/w300/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
+    synopsis: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s.',
+    trailer: 'https://www.youtube.com/embed/psdj8kT6N6U'
+  },
+  {
+    id: '2',
+    title: 'Joker',
+    year: '2019',
+    poster: 'https://image.tmdb.org/t/p/w300/q719jXXEzOoYaps6babgKnONONX.jpg',
+    synopsis: 'In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society.',
+    trailer: 'https://www.youtube.com/embed/zAGVQLHvwOY'
+  },
+  {
+    id: '3',
+    title: 'Suicide Squad',
+    year: '2016',
+    poster: 'https://image.tmdb.org/t/p/w300/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg',
+    synopsis: 'A secret government agency recruits imprisoned supervillains to execute dangerous black ops missions.',
+    trailer: 'https://www.youtube.com/embed/Exv_8yUE2fc'
+  }
+];
